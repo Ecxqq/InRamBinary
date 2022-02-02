3ab4d78f0a82f131ca41a1eec0ed6bebe261fca6
Ecxqq <kurbatovaleksandr01@gmail.com>
2022-02-02 15:20:37 +0700

    binaryInRam

---
+++ MainForm.Designer.cs
@@ -0,0 +1,292 @@
+﻿
+namespace Kursovaya7a
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.RadioButton sbyte1;
+		private System.Windows.Forms.RadioButton short1;
+		private System.Windows.Forms.RadioButton int1;
+		private System.Windows.Forms.RadioButton long1;
+		private System.Windows.Forms.RadioButton ulong1;
+		private System.Windows.Forms.RadioButton uint1;
+		private System.Windows.Forms.RadioButton ushort1;
+		private System.Windows.Forms.RadioButton byte1;
+		private System.Windows.Forms.RadioButton decimal1;
+		private System.Windows.Forms.RadioButton double1;
+		private System.Windows.Forms.RadioButton float1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.RichTextBox richTextBox1;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Panel panel2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label2;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.sbyte1 = new System.Windows.Forms.RadioButton();
+			this.short1 = new System.Windows.Forms.RadioButton();
+			this.int1 = new System.Windows.Forms.RadioButton();
+			this.long1 = new System.Windows.Forms.RadioButton();
+			this.ulong1 = new System.Windows.Forms.RadioButton();
+			this.uint1 = new System.Windows.Forms.RadioButton();
+			this.ushort1 = new System.Windows.Forms.RadioButton();
+			this.byte1 = new System.Windows.Forms.RadioButton();
+			this.decimal1 = new System.Windows.Forms.RadioButton();
+			this.double1 = new System.Windows.Forms.RadioButton();
+			this.float1 = new System.Windows.Forms.RadioButton();
+			this.button1 = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.panel1.SuspendLayout();
+			this.panel2.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// sbyte1
+			// 
+			this.sbyte1.Location = new System.Drawing.Point(17, 74);
+			this.sbyte1.Name = "sbyte1";
+			this.sbyte1.Size = new System.Drawing.Size(104, 24);
+			this.sbyte1.TabIndex = 0;
+			this.sbyte1.TabStop = true;
+			this.sbyte1.Text = "sbyte";
+			this.sbyte1.UseVisualStyleBackColor = true;
+			// 
+			// short1
+			// 
+			this.short1.Location = new System.Drawing.Point(17, 104);
+			this.short1.Name = "short1";
+			this.short1.Size = new System.Drawing.Size(104, 24);
+			this.short1.TabIndex = 1;
+			this.short1.TabStop = true;
+			this.short1.Text = "short";
+			this.short1.UseVisualStyleBackColor = true;
+			// 
+			// int1
+			// 
+			this.int1.Location = new System.Drawing.Point(17, 134);
+			this.int1.Name = "int1";
+			this.int1.Size = new System.Drawing.Size(104, 24);
+			this.int1.TabIndex = 2;
+			this.int1.TabStop = true;
+			this.int1.Text = "int";
+			this.int1.UseVisualStyleBackColor = true;
+			// 
+			// long1
+			// 
+			this.long1.Location = new System.Drawing.Point(17, 164);
+			this.long1.Name = "long1";
+			this.long1.Size = new System.Drawing.Size(104, 24);
+			this.long1.TabIndex = 3;
+			this.long1.TabStop = true;
+			this.long1.Text = "long";
+			this.long1.UseVisualStyleBackColor = true;
+			// 
+			// ulong1
+			// 
+			this.ulong1.Location = new System.Drawing.Point(127, 164);
+			this.ulong1.Name = "ulong1";
+			this.ulong1.Size = new System.Drawing.Size(104, 24);
+			this.ulong1.TabIndex = 7;
+			this.ulong1.TabStop = true;
+			this.ulong1.Text = "ulong";
+			this.ulong1.UseVisualStyleBackColor = true;
+			// 
+			// uint1
+			// 
+			this.uint1.Location = new System.Drawing.Point(127, 134);
+			this.uint1.Name = "uint1";
+			this.uint1.Size = new System.Drawing.Size(104, 24);
+			this.uint1.TabIndex = 6;
+			this.uint1.TabStop = true;
+			this.uint1.Text = "uint";
+			this.uint1.UseVisualStyleBackColor = true;
+			// 
+			// ushort1
+			// 
+			this.ushort1.Location = new System.Drawing.Point(127, 104);
+			this.ushort1.Name = "ushort1";
+			this.ushort1.Size = new System.Drawing.Size(104, 24);
+			this.ushort1.TabIndex = 5;
+			this.ushort1.TabStop = true;
+			this.ushort1.Text = "ushort";
+			this.ushort1.UseVisualStyleBackColor = true;
+			// 
+			// byte1
+			// 
+			this.byte1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+			this.byte1.Location = new System.Drawing.Point(127, 74);
+			this.byte1.Name = "byte1";
+			this.byte1.Size = new System.Drawing.Size(104, 24);
+			this.byte1.TabIndex = 4;
+			this.byte1.TabStop = true;
+			this.byte1.Text = "byte";
+			this.byte1.UseVisualStyleBackColor = true;
+			// 
+			// decimal1
+			// 
+			this.decimal1.Location = new System.Drawing.Point(17, 289);
+			this.decimal1.Name = "decimal1";
+			this.decimal1.Size = new System.Drawing.Size(104, 24);
+			this.decimal1.TabIndex = 10;
+			this.decimal1.TabStop = true;
+			this.decimal1.Text = "decimal";
+			this.decimal1.UseVisualStyleBackColor = true;
+			// 
+			// double1
+			// 
+			this.double1.Location = new System.Drawing.Point(17, 259);
+			this.double1.Name = "double1";
+			this.double1.Size = new System.Drawing.Size(104, 24);
+			this.double1.TabIndex = 9;
+			this.double1.TabStop = true;
+			this.double1.Text = "double";
+			this.double1.UseVisualStyleBackColor = true;
+			// 
+			// float1
+			// 
+			this.float1.Location = new System.Drawing.Point(17, 229);
+			this.float1.Name = "float1";
+			this.float1.Size = new System.Drawing.Size(104, 24);
+			this.float1.TabIndex = 8;
+			this.float1.TabStop = true;
+			this.float1.Text = "float";
+			this.float1.UseVisualStyleBackColor = true;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(6, 152);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(122, 24);
+			this.button1.TabIndex = 12;
+			this.button1.Text = "Перевод";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(3, 28);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(273, 20);
+			this.textBox1.TabIndex = 13;
+			this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBox1KeyPress);
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(3, 12);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(100, 13);
+			this.label1.TabIndex = 14;
+			this.label1.Text = "Введите число:";
+			// 
+			// richTextBox1
+			// 
+			this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+			| System.Windows.Forms.AnchorStyles.Right)));
+			this.richTextBox1.Location = new System.Drawing.Point(6, 22);
+			this.richTextBox1.Name = "richTextBox1";
+			this.richTextBox1.ReadOnly = true;
+			this.richTextBox1.Size = new System.Drawing.Size(230, 124);
+			this.richTextBox1.TabIndex = 15;
+			this.richTextBox1.Text = "";
+			// 
+			// panel1
+			// 
+			this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+			| System.Windows.Forms.AnchorStyles.Left)));
+			this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+			this.panel1.Controls.Add(this.label3);
+			this.panel1.Controls.Add(this.label2);
+			this.panel1.Controls.Add(this.label1);
+			this.panel1.Controls.Add(this.textBox1);
+			this.panel1.Controls.Add(this.double1);
+			this.panel1.Controls.Add(this.decimal1);
+			this.panel1.Controls.Add(this.sbyte1);
+			this.panel1.Controls.Add(this.short1);
+			this.panel1.Controls.Add(this.float1);
+			this.panel1.Controls.Add(this.int1);
+			this.panel1.Controls.Add(this.ulong1);
+			this.panel1.Controls.Add(this.long1);
+			this.panel1.Controls.Add(this.uint1);
+			this.panel1.Controls.Add(this.byte1);
+			this.panel1.Controls.Add(this.ushort1);
+			this.panel1.Location = new System.Drawing.Point(12, 12);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(288, 342);
+			this.panel1.TabIndex = 16;
+			// 
+			// label3
+			// 
+			this.label3.Location = new System.Drawing.Point(17, 215);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(138, 11);
+			this.label3.TabIndex = 16;
+			this.label3.Text = "Вещественные типы: ";
+			// 
+			// label2
+			// 
+			this.label2.Location = new System.Drawing.Point(17, 58);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(161, 13);
+			this.label2.TabIndex = 15;
+			this.label2.Text = "Целочисленные типы: ";
+			// 
+			// panel2
+			// 
+			this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+			| System.Windows.Forms.AnchorStyles.Left) 
+			| System.Windows.Forms.AnchorStyles.Right)));
+			this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+			this.panel2.Controls.Add(this.richTextBox1);
+			this.panel2.Controls.Add(this.button1);
+			this.panel2.Location = new System.Drawing.Point(306, 12);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(246, 342);
+			this.panel2.TabIndex = 17;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(564, 360);
+			this.Controls.Add(this.panel2);
+			this.Controls.Add(this.panel1);
+			this.Name = "MainForm";
+			this.Text = "Kursovaya7a";
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			this.panel2.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+	}
+}
---
+++ MainForm.cs
@@ -0,0 +1,400 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+
+namespace Kursovaya7a
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		void Button1Click(object sender, EventArgs e)
+		{
+			if (byte1.Checked==true)
+			{
+                try 
+                { 
+                	Convert.ToByte(textBox1.Text);
+                    unsafe 
+                    {  
+                    	string st=""; 
+                    	richTextBox1.Clear();
+                        byte y = Convert.ToByte(textBox1.Text);
+                        byte* x = &y;
+                        uint adres = (uint)x;
+                        byte* znach = (byte*)adres;
+                        for (int count =0; count<1; count++) //количество байт
+                        {
+                        	string    g = Convert.ToString(*(znach+count),2);
+                        	char [] g1 = g.ToCharArray();
+                        	Array.Reverse(g1);
+                        	string g2=new string (g1);
+                        	while (g2.Length<8){g2=g2+'0';}
+                        	st+="  "+g2;
+                        }
+                        char [] st1 = st.ToCharArray();
+                        Array.Reverse(st1);
+                        string st2 = new string(st1);
+                        richTextBox1.Text=""+st2;
+                    }
+                    
+                } catch
+                {
+                    MessageBox.Show("Ошибка во введенных данных");
+                }
+            }
+            if (sbyte1.Checked==true) 
+            {
+                try 
+                { 
+                	Convert.ToSByte(textBox1.Text);
+                    unsafe 
+                    {  
+                    	string st=""; 
+                    	richTextBox1.Clear();
+                        sbyte y = Convert.ToSByte(textBox1.Text);
+                        sbyte *x = &y;
+                        void* adres = x;
+                        byte* znach = (byte*)adres;
+                        for (int count =0; count<1; count++) //количество байт
+                        {
+                        	string    g = Convert.ToString(*(znach+count),2);
+                        	char [] g1 = g.ToCharArray();
+                        	Array.Reverse(g1);
+                        	string g2=new string (g1);
+                        	while (g2.Length<8){g2=g2+'0';}
+                        	st+="  "+g2;
+                        }
+                        char [] st1 = st.ToCharArray();
+                        Array.Reverse(st1);
+                        string st2 = new string(st1);
+                        richTextBox1.Text=""+st2;
+                    }
+                    
+                } 
+                catch
+                {
+                    MessageBox.Show("Ошибка во введенных данных");
+                }
+            }
+            if (short1.Checked==true) 
+            {
+                try 
+                { 
+                	Convert.ToInt16(textBox1.Text);
+                    unsafe 
+                    {  
+                    	string st=""; 
+                    	richTextBox1.Clear();
+                        short y = Convert.ToInt16(textBox1.Text);
+                        short *x = &y;
+                        void* adres = x;
+                        byte* znach = (byte*)adres;
+                        for (int count =0; count<2; count++) //количество байт
+                        {
+                        	string    g = Convert.ToString(*(znach+count),2);
+                        	char [] g1 = g.ToCharArray();
+                        	Array.Reverse(g1);
+                        	string g2=new string (g1);
+                        	while (g2.Length<8){g2=g2+'0';}
+                        	st+="  "+g2;
+                        }
+                        char [] st1 = st.ToCharArray();
+                        Array.Reverse(st1);
+                        string st2 = new string(st1);
+                        richTextBox1.Text=""+st2;
+                    }
+                    
+                } catch
+                {
+                    MessageBox.Show("Ошибка во введенных данных");
+                }
+            }
+            if (ushort1.Checked==true) 
+            {
+                try 
+                { 
+                	Convert.ToUInt16(textBox1.Text);
+                    unsafe 
+                    {
+                    	string st=""; 
+                    	richTextBox1.Clear();
+                        ushort y = Convert.ToUInt16(textBox1.Text);
+                        ushort *x = &y;
+                        void* adres = x;
+                        byte* znach = (byte*)adres;
+                        for (int count =0; count<2; count++) //количество байт
+                        {
+                            string    g = Convert.ToString(*(znach+count),2);
+                        	char [] g1 = g.ToCharArray();
+                        	Array.Reverse(g1);
+                        	string g2=new string (g1);
+                        	while (g2.Length<8){g2=g2+'0';}
+                        	st+="  "+g2;
+                        }
+                        char [] st1 = st.ToCharArray();
+                        Array.Reverse(st1);
+                        string st2 = new string(st1);
+                        richTextBox1.Text=""+st2;
+                    }
+                    
+                } catch
+                {
+                   	MessageBox.Show("Ошибка во введенных данных");
+                }
+            }
+            if (int1.Checked==true) 
+            {
+                try 
+                {
+					Convert.ToInt32(textBox1.Text);
+                    unsafe 
+                    {
+                    	string st="";
+                    	richTextBox1.Clear();
+                        int y = Convert.ToInt16(textBox1.Text);
+                        int *x = &y;
+                        void* adres = x;
+                        byte* znach = (byte*)adres;
+                        for (int count =0; count<4; count++) //количество байт
+                        {
+                            string    g = Convert.ToString(*(znach+count),2);
+                        	char [] g1 = g.ToCharArray();
+                        	Array.Reverse(g1);
+                        	string g2=new string (g1);
+                        	while (g2.Length<8){g2=g2+'0';}
+                        	st+="  "+g2;
+                        }
+                        char [] st1 = st.ToCharArray();
+                        Array.Reverse(st1);
+                        string st2 = new string(st1);
+                        richTextBox1.Text=""+st2; 
+                    }
+                    
+                } catch
+                {
+                    MessageBox.Show("Ошибка во введенных данных");
+                }
+            }
+            if (uint1.Checked==true) 
+            {
+                try 
+                { 
+                	Convert.ToUInt32(textBox1.Text);
+                    unsafe 
+                    {
+                    	string st="";
+                    	richTextBox1.Clear();
+                        uint y = Convert.ToUInt32(textBox1.Text);
+                        uint *x = &y;
+                        void* adres = x;
+                        byte* znach = (byte*)adres;
+                        for (int count =0; count<4; count++) //количество байт
+                        {
+                            string    g = Convert.ToString(*(znach+count),2);
+                        	char [] g1 = g.ToCharArray();
+                        	Array.Reverse(g1);
+                        	string g2=new string (g1);
+                        	while (g2.Length<8){g2=g2+'0';}
+                        	st+="  "+g2;
+                        }
+                        char [] st1 = st.ToCharArray();
+                        Array.Reverse(st1);
+                        string st2 = new string(st1);
+                        richTextBox1.Text=""+st2;
+                    }
+                    
+                } catch
+                {
+                    MessageBox.Show("Ошибка во введенных данных");
+                }
+            }
+            if (long1.Checked==true) 
+            {
+                try 
+                {
+					Convert.ToInt64(textBox1.Text);
+                    unsafe 
+                    {
+                    	string st="";
+                    	richTextBox1.Clear();
+                        long y = Convert.ToInt64(textBox1.Text);
+                        long *x = &y;
+                        void* adres = x;
+                        byte* znach = (byte*)adres;
+                        for (int count =0; count<8; count++) //количество байт
+                        {
+                            string    g = Convert.ToString(*(znach+count),2);
+                        	char [] g1 = g.ToCharArray();
+                        	Array.Reverse(g1);
+                        	string g2=new string (g1);
+                        	while (g2.Length<8){g2=g2+'0';}
+                        	st+=" "+g2;
+                        }
+                        char [] st1 = st.ToCharArray();
+                        Array.Reverse(st1);
+                        string st2 = new string(st1);
+                        richTextBox1.Text=""+st2;  
+                    }
+                    
+                } catch
+                {
+                    MessageBox.Show("Ошибка во введенных данных");
+                }
+            }
+            if (ulong1.Checked==true) 
+            {
+                try 
+                {
+					Convert.ToUInt64(textBox1.Text);
+                    unsafe 
+                    {  
+                    	string st="";
+                    	richTextBox1.Clear();
+                        ulong y = Convert.ToUInt64(textBox1.Text);
+                        ulong *x = &y;
+                        void* adres = x;
+                        byte* znach = (byte*)adres;
+                        for (int count =0; count<8; count++) //количество байт
+                        {
+                        	string    g = Convert.ToString(*(znach+count),2);
+                        	char [] g1 = g.ToCharArray();
+                        	Array.Reverse(g1);
+                        	string g2=new string (g1);
+                        	while (g2.Length<8){g2=g2+'0';}
+                        	st+=" "+g2;
+                        }
+                        char [] st1 = st.ToCharArray();
+                        Array.Reverse(st1);
+                        string st2 = new string(st1);
+                        richTextBox1.Text=""+st2;
+                    }
+                    
+                } catch
+                {
+                    MessageBox.Show("Ошибка во введенных данных");
+                }
+            }
+            if (float1.Checked==true) { 
+                try 
+                { 
+                	Convert.ToSingle(textBox1.Text);
+                    unsafe 
+                    {  
+                    	string st=""; 
+                    	richTextBox1.Clear();
+                        float y = Convert.ToSingle(textBox1.Text);
+                        float *x = &y;
+                        void* adres = x;
+                        byte* znach = (byte*)adres;
+                        for (int count =0; count<4; count++) //количество байт
+                        {
+                        	string    g = Convert.ToString(*(znach+count),2);
+                        	char [] g1 = g.ToCharArray();
+                        	Array.Reverse(g1);
+                        	string g2=new string (g1);
+                        	while (g2.Length<8){g2=g2+'0';}
+                        	st+="  "+g2;
+                        }
+                        char [] st1 = st.ToCharArray();
+                        Array.Reverse(st1);
+                        string st2 = new string(st1);
+                        richTextBox1.Text=""+st2;
+                     }
+                    
+                } catch
+                {
+                    MessageBox.Show("Ошибка во введенных данных");
+                }
+            }
+            if (double1.Checked==true) 
+            {
+                try 
+                { 
+                	Convert.ToSingle(textBox1.Text);
+                    unsafe 
+                    {  
+                    	string st=""; 
+                    	richTextBox1.Clear();
+                        double y = Convert.ToSingle(textBox1.Text);
+                        double *x = &y;
+                        void* adres = x;
+                        byte* znach = (byte*)adres;
+                        for (int count =0; count<8; count++) //количество байт
+                        {
+                        	string    g = Convert.ToString(*(znach+count),2);
+                        	char [] g1 = g.ToCharArray();
+                        	Array.Reverse(g1);
+                        	string g2=new string (g1);
+                        	while (g2.Length<8){g2=g2+'0';}
+                        	st+="  "+g2;
+                        }
+                        char [] st1 = st.ToCharArray();
+                        Array.Reverse(st1);
+                        string st2 = new string(st1);
+                        richTextBox1.Text=""+st2;
+                     }
+                    
+                } catch
+                {
+                    MessageBox.Show("Ошибка во введенных данных");
+                }
+            }
+            if (decimal1.Checked==true) 
+            {
+                try 
+                { 
+                	Convert.ToDecimal(textBox1.Text);
+                    unsafe 
+                    {  
+                    	string st=""; 
+                    	richTextBox1.Clear();
+                        decimal y = Convert.ToDecimal(textBox1.Text);
+                        decimal *x = &y;
+                        void* adres = x;
+                        byte* znach = (byte*)adres;
+                        for (int count =0; count<10; count++) //количество байт
+                        {
+                        	string    g = Convert.ToString(*(znach+count),2);
+                        	char [] g1 = g.ToCharArray();
+                        	Array.Reverse(g1);
+                        	string g2=new string (g1);
+                        	while (g2.Length<8){g2=g2+'0';}
+                        	st+="  "+g2;
+                        }
+                        char [] st1 = st.ToCharArray();
+                        Array.Reverse(st1);
+                        string st2 = new string(st1);
+                        richTextBox1.Text=""+st2;
+                    }
+                    
+                } catch
+                {
+                    MessageBox.Show("Ошибка во введенных данных");
+                }
+            }
+        }
+		
+		void TextBox1KeyPress(object sender, KeyPressEventArgs e)
+		{
+			char number = e.KeyChar;
+			if (!Char.IsDigit(number) && number != 8 && number != 44 && number != '-') 
+			{
+    			e.Handled = true;
+			}
+		}
+    }
+}
